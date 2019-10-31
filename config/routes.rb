c9170ed6b5588c7a2db1a9ef061de449d3195e19
protomelon <melaniemop@gmail.com>
2019-10-31 05:24:25 -0500

    Navigation menu for all views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :pages
-  get 'pages/about'
+  root to: 'pages#about'
+
+  get 'pages/about', to: 'pages#about', as: 'about'
+  resources :pages, only: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
