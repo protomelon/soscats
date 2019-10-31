7ca22d91218234699c56ea6146675ee39eb1ecab
protomelon <melaniemop@gmail.com>
2019-10-31 05:32:07 -0500

    Updated cat controller

--- app/controllers/cat_controller.rb
+++ app/controllers/cat_controller.rb
@@ -1,7 +1,10 @@
 class CatController < ApplicationController
   def index
+    @cats = Cat.all
+    @total_cats = Cat.all.count
   end
 
   def show
+    @cat = Cat.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'cat/index'
-  get 'cat/show'
-  get 'breed/index'
-  get 'breed/show'
-  get 'trees/index'
-  get 'trees/show'
-  root to: 'pages#about'
+  root to: 'cat#index'
 
+  resources :pages
   get 'pages/about', to: 'pages#about', as: 'about'
-  resources :pages, only: :about
+
+  resources :trees, only: %i[index show]
+  resources :breeds, only: %i[index show]
+  resources :cats, only: %i[index show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
